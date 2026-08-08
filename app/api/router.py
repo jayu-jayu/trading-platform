512d6acad6dde35ef6eeed209e7053e9beda175b
Jayesh Ravindra Chaudhari <75301812+jayu-jayu@users.noreply.github.com>
2026-08-08 20:49:52 +0530

    Delete app directory

--- app/api/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api import signals, prices, portfolio, backtest
-
-api_router = APIRouter()
-api_router.include_router(signals.router)
-api_router.include_router(prices.router)
-api_router.include_router(portfolio.router)
-api_router.include_router(backtest.router)
